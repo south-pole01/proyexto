0604b7c4f5153c3108aa45df3aa5ca5cf1950c31
Gonzalo Venegas <vamosquesepuedee@gmail.com>
2021-07-08 13:00:23 -0400

    Mejoras visuales con bootstrap

--- js/validador.js
+++ js/validador.js
@@ -7,8 +7,12 @@ $("#formulario1").validate({
         "txtContrasenia": {
             required: true,
             minlength: 5
-        }
+        },
 
+        "txtRepetirContra": {
+            required: true,
+            equalTo: '#id_txtContrasenia'
+                    }
 
     },
 
@@ -20,7 +24,11 @@ $("#formulario1").validate({
         "txtContrasenia":{
             required: 'Ingresa la contrasenia!!',
             minlength: 'Minimo 5 caracteres'
-        }
+        },
+        "txtRepetirContra": {
+            required: 'Repita el Password!!',
+            equalTo: ' deben ser iguales!!!!'
+                    }        
 
     }
 })
